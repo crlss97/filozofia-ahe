4d7e4bdbb7d37520b7d898d0436ffd2d181fe099
Karol Zaremba <karol.zaremba06@gmail.com>
2022-01-23 17:38:50 +0100

    Finalizing quiz.
    Add questions, verification and summarize points.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
-import {NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuestionPanelComponent } from './question-panel/question-panel.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SummaryModalComponent } from './summary-modal/summary-modal.component';
+import {SummaryDialogModule} from "./summary-dialog.module";
+import {MatExpansionModule} from "@angular/material/expansion";
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuestionPanelComponent
+    QuestionPanelComponent,
+    SummaryModalComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    SummaryDialogModule,
+    BrowserAnimationsModule,
+    MatExpansionModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
+  entryComponents: [SummaryModalComponent]
 })
 export class AppModule { }
